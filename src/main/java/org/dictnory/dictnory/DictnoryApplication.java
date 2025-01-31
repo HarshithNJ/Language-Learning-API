036a9ddd67a635df23a91a03781f9c19355f74c9
Harshith N J <njharshith122@gmail.com>
2025-01-31 11:43:27 +0530

    Started the Project

---
+++ src/main/java/org/dictnory/dictnory/DictnoryApplication.java
@@ -0,0 +1,13 @@
+package org.dictnory.dictnory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictnoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictnoryApplication.class, args);
+	}
+
+}
