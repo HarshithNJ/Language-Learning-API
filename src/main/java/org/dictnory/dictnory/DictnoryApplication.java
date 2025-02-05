ecd070c92fd8d99f88d6c71f0fe2543bab76cf21
Harshith N J <njharshith122@gmail.com>
2025-02-05 14:06:44 +0530

    swagger dependency injection

--- src/main/java/org/dictnory/dictnory/DictnoryApplication.java
+++ src/main/java/org/dictnory/dictnory/DictnoryApplication.java
@@ -3,6 +3,11 @@ package org.dictnory.dictnory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+@OpenAPIDefinition(info = @Info(title = "Dictnory API", version = "1.0", description = "Dictnory API", 
+					contact = @Contact(name = "Harshith N J", email = "Harshith.dev2024@outlook.com")))
 @SpringBootApplication
 public class DictnoryApplication {
 
